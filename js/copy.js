532a616e4d30d8914309bb081ee211a62178c0f3
Diego Paixão <diego.spaixao91@hotmail.com>
2023-05-10 21:05:31 -0300

    Update copy.js

--- js/copy.js
+++ js/copy.js
@@ -1,6 +1,6 @@
 const currentYear = new Date().getFullYear();
 const copyrightElement = document.querySelector('#copyright');
-const appTitle = 'PokeGuide';
+const appTitle = 'Gorjeta';
 
 copyrightElement.innerHTML =
   `&copy; ${currentYear} ${appTitle}`;
