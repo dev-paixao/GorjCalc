adfc6d83a1719a75e34769a98cd934092e45f9fd
Diego Paixão <diego.spaixao91@hotmail.com>
2023-05-19 18:42:02 -0300

    Delete copy.js

--- js/copy.js
+++
@@ -1,6 +0,0 @@
-const currentYear = new Date().getFullYear();
-const copyrightElement = document.querySelector('#copyright');
-const appTitle = 'Gorjeta';
-
-copyrightElement.innerHTML =
-  `&copy; ${currentYear} ${appTitle}`;
